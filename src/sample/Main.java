4761ad16a9f37043299752544650a5c7bfe7a421
elka74 <mikheeva@1454.ru>
2020-05-27 02:47:05 +0300

    calculator

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Калькулятор");
+        primaryStage.setScene(new Scene(root, 375, 450));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
